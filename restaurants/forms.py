ad6a37f3d3d0554fde21f329ad72bacc0bfc8337
Kashif <kashifshahzad@yours.com>
2020-01-20 21:09:36 +0300

    task

---
+++ restaurants/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from .models import Restaurant, Item
+from django.contrib.auth.models import User
+from .forms import Item
+
+class RestaurantForm(forms.ModelForm):
+    class Meta:
+        model = Restaurant
+        fields = ['name', 'description', 'opening_time', 'closing_time','logo']
+
+        widgets = {
+        	'opening_time': forms.TimeInput(attrs={'type':'time'}),
+        	'closing_time': forms.TimeInput(attrs={'type':'time'}),
+        }
+
+
+class SignupForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email' ,'password']
+
+        widgets={
+        'password': forms.PasswordInput(),
+        }
+
+class SigninForm(forms.Form):
+    username = forms.CharField(required=True)
+    password = forms.CharField(required=True, widget=forms.PasswordInput())
+
+
+class ItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields= ['name','description', 'price']
---
+++ restaurants/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField()
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
+    logo = models.ImageField(upload_to='restaurant_logos', null=True, blank=True)
+    owner=models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    name= models.CharField(max_length=120)
+    description= models.TextField()
+    price= models.DecimalField(decimal_places=2, max_digits=5)
+    restaurant= models.ForeignKey(Restaurant, on_delete=models.CASCADE, null=True) 
+
+    def __str__(self):
+        return self.name
